fec88b557057ff716ae1ccc79735d9a7fdf8da9e
François Chatelier <francois.chatelier@gmail.com>
2021-11-22 15:32:12 +0100

    Récup du Repo React_Jokes + réalisation des 5 premières étapes

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Joke from './components/Joke';
+
+function App() {
+  return (
+    <div className="App">
+      <Joke {blagues.setup} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Joke.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Joke.css';
+
+const Joke = ({ blague }) => {
+  console.log(blague);
+  return (
+    <div className="design">
+      <h1>Blague</h1>
+    </div>
+  );
+};
+
+export default Joke;
