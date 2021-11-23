9514fe0b8a24b5839d684ad4eb97dd778ac0f436
François Chatelier <francois.chatelier@gmail.com>
2021-11-23 12:44:17 +0100

    Step 20 // récup de l'API

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,40 @@
+import React, { useState } from 'react';
 import Joke from './components/Joke';
+import Filter from './components/Filter';
 import blagues from './data/jokes';
 
 function App() {
-  console.log(blagues);
+  const [numberJokes, setNumberJokes] = useState(1);
+  const [withDelivery, setWithDelivery] = useState(true);
+  const [containsWord, setContainsWord] = useState('');
+  const [emptyWord, setEmptyWord] = useState('');
+  console.log(setEmptyWord);
+
+  // const toggleWithDelivery = (event) => {
+  // setWithDelivery(event.target.value) ou setWithDelivery(!withDelivery);
+  // };
+
+  const getApiCall = () => {
+    let url = `$https://v2.jokeapi.dev/joke/Any?&type=''`;
+    url += withDelivery ? 'single' : 'twoparts';
+    url += `&amount=${numberJokes}`;
+    url += emptyWord ? `&contains=${emptyWord}` : '';
+    return url;
+  };
+  console.log(getApiCall);
+
   return (
-    <div className="App">
+    <div>
+      <Filter
+        numberJokes={numberJokes}
+        setNumberJokes={setNumberJokes}
+        withDelivery={withDelivery}
+        setWithDelivery={
+          setWithDelivery
+        } /** ou remplacer par function toggleWithDelivery */
+        containsWord={containsWord}
+        setContainsWord={setContainsWord}
+      />
       {blagues.map((blague) => (
         <Joke blague={blague} chute="Ceci est la chute" />
       ))}
---
+++ src/components/Filter.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import './filter.css';
+
+const Filter = ({
+  numberJokes,
+  setNumberJokes,
+  containsWord,
+  setContainsWord,
+  withDelivery,
+  setWithDelivery,
+}) => {
+  console.log(Filter);
+  return (
+    <div className="navbar">
+      <div>
+        <label className="blague" htmlFor="quantity">
+          Nombre de blagues:
+          <input
+            type="number"
+            id="quantity"
+            name="quantity"
+            value={numberJokes} /** Permet d'ajouter de la valeur à l'input */
+            onChange={(event) =>
+              setNumberJokes(event.target.value)
+            } /* Ecouter l'input */
+          />
+        </label>
+      </div>
+      <div>
+        <label className="chute" htmlFor="chute">
+          Avec Chute:
+          <input
+            type="checkbox"
+            value={withDelivery}
+            onClick={(event) =>
+              setWithDelivery(event.target.value)
+            } /** ou remplacer par function toggleWithDelivery */
+          />
+        </label>
+      </div>
+      <div>
+        <label htmlFor="text">
+          Contient:
+          <input
+            type="text"
+            value={containsWord}
+            onChange={(event) => setContainsWord(event.target.value)}
+          />
+        </label>
+      </div>
+    </div>
+  );
+};
+
+export default Filter;
